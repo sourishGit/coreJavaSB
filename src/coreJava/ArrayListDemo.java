adf33c103bdf5f6c880ca8b809d2e5bd94b29152
sourish <sourish.vlogger@gmail.com>
2025-07-21 16:55:10 +0530

    ArrayList topic

---
+++ src/coreJava/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package coreJava;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Text");
+		al.add("Sourish");
+		al.add("Modu");
+		al.add(0, "England");
+		System.out.println(al);
+		System.out.println(al.get(2));
+		System.out.println(al.contains("Text"));
+		al.remove(1);
+		System.out.println(al);
+		al.remove("Modu");
+		System.out.println(al);
+	}
+
+}
