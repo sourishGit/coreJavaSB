83f41702bad75c475695ed90e666d735b943d1bd
sourish <sourish.vlogger@gmail.com>
2025-07-21 16:43:47 +0530

    commit packages

---
+++ src/coreJava/democlass1.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public class democlass1 {
+	
+	public void test()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/demopackage/demoClass.java
@@ -0,0 +1,17 @@
+package demopackage;
+
+import coreJava.democlass1;
+
+public class demoClass  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		democlass1 d = new democlass1();
+		d.test();
+		
+		
+
+	}
+
+}
